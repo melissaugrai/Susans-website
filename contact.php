d6f710fedec9632a14d6ba4009df446896e0b5ff
Melissa Ugrai <mugrai@gmail.com>
2021-11-01 16:07:31 -0700

    Test number 2 of contact form.

--- contact.php
+++ contact.php
@@ -1,26 +1,20 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
+<?php 
+if(isset($_POST['submit'])){
+    $to = "mugrai@gmail.com"; // this is your Email address
+    $from = $_POST['visitor_email']; // this is the sender's Email address
+    $client_name = $_POST['visitor_name'];
+    $client_email = $_POST['visitor_email'];
 
- */
-require_once './vendor/autoload.php';
+    $subject = "Form submission";
+    $message = $client_name . " " . $client_email . " wrote the following:" . "\n\n" . $_POST['visitor_message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('mugrai@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST); 
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $client_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
