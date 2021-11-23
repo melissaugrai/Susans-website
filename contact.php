0c817840c3dab029c3a29283956069ac2505b8c1
Melissa Ugrai <mugrai@gmail.com>
2021-11-23 14:27:44 -0800

    Update testimonial section. Update contact form PHP

--- contact.php
+++ contact.php
@@ -3,18 +3,18 @@
 THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
 */
 
-require 'PHPMailer-master/PHPMailerAutoload.php';
+require 'src/PHPMailer-master/PHPMailerAutoload.php';
 
 /*
 *  CONFIGURE EVERYTHING HERE
 */
 
 // an email address that will be in the From field of the email.
-$fromEmail = 'demo@domain.com';
+$fromEmail = 'email';
 $fromName = 'Demo contact form';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'demo@domain.com';
+$sendToEmail = 'mugrai@gmail.com';
 $sendToName = 'Demo contact form';
 
 // subject of the email
@@ -22,7 +22,7 @@ $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
