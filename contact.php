41936dddab89f4c99779c1cb7a7901b8a2cd5589
Melissa Ugrai <mugrai@gmail.com>
2021-12-11 19:29:31 -0800

    Got Form to work Correctly

--- contact.php
+++ contact.php
@@ -1,71 +1,31 @@
+<?php 
 
-<?php // Initialize variables to null.
-$name =""; // Sender Name
-$email =""; // Sender's email ID
-$message =""; // Sender's Message
-$nameError ="";
-$emailError ="";
-$messageError ="";
-$successMessage =""; // On submitting form below function will execute.
-if(isset($_POST['submit'])) { // Checking null values in message.
-if (empty($_POST["name"])){
-$nameError = "Name is required";
-}
-else
- {
-$name = test_input($_POST["name"]); // check name only contains letters and whitespace
-if (!preg_match("/^[a-zA-Z ]*$/",$name))
-{
-$nameError = "Only letters and white space allowed";
-}
-} // Checking null values in the message.
-if (empty($_POST["email"]))
-{
-$emailError = "Email is required";
-}
-else
- {
-$email = test_input($_POST["email"]);
-} 
-// Checking null values in message.
-if (empty($_POST["message"]))
-{
-$messageError = "Message is required";
-}
-else
- {
-$message = test_input($_POST["message"]);
-} // Checking null values in the message.
-if( !($name=='') && !($email=='') &&!($message=='') )
-{ // Checking valid email.
-if (preg_match("/([w-]+@[w-]+.[w-]+)/",$email))
-{
-$header= $name."<". $email .">";
-$headers = "Susan's Optimum Health Goals"; /* Let's prepare the message for the e-mail */
-$msg = "Hello! $name Thank you...! For Contacting Us.
-Name: $name
-E-mail: $email
-Message: $message
-This is a Contact Confirmation mail. We Will contact You as soon as possible.";
-$msg1 = " $name Contacted Us. Here is some information about $name.
-Name: $name
-E-mail: $email
-Message: $message "; /* Send the message using mail() function */
-if(mail($email, $headers, $msg ) && mail("mugrai@gmail.com", $header, $msg1 ))
-{
-$successMessage = "Message sent successfully.......";
-}
-}
-else
-{ $emailError = "Invalid Email";
+if (isset($_POST['email']) && $_POST['email'] !="") {
+
+     if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    //submit the form
+        $userName = $_POST['name'];
+        $userEmail = $_POST['email'];
+        $userMessage = $_POST['message'];
+             
+        $to = "mugrai@gmail.com";
+        $header = "New Form Submitted";
+        $body = "";
+
+        $body .= "From: ".$userName. "\r\n";
+        $body .= "Email: ".$userEmail. "\r\n";
+        $body .= "Message: ".$userMessage. "\r\n";
+
+        mail($to, $header, $body);
+
+        echo "Thank you $userName, I will be in touch shortly.";	  
+        
+    } else {
+
+	  echo 'Please provide your name and email address.';
     }
- }
+
 }
-// Function for filtering input values.function test_input($data)
-function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-}
+
 ?>
+
